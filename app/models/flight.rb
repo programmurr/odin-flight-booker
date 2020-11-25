7453f8e8851ba3ba68a6094592145baac96e3080
Conor <conorjm89@gmail.com>
2020-11-25 23:13:37 +0000

    Fixed error showing multiple flight dates

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,10 +5,6 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, class_name: 'Airport'
 
   def self.get_flight_dates
-    flight_dates = Flight.find_by_sql('select distinct start from flights where start is not null order by start asc')
-  end
-
-  def flight_date_formatted
-    start.strftime('%d/%m/%Y')
+    Flight.pluck(:start).map { |date| [date.strftime('%d/%m/%Y')] }.uniq
   end
 end
